b1b2d5225fa32fe9ac66bb0260447a582d40a5b0
augustine <94568395+xaaug@users.noreply.github.com>
2024-11-15 17:31:44 +0300

    Initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { watchFile } = require('fs')
+
+module.exports = {
+    mode: "production",
+    entry: './src/index.js',
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ['./src/page.html']
+    },
+    plugins: [
+       new HTMLWebpackPlugin({
+        template: "./src/page.html"
+       })
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader", "postcss-loader"]
+          },
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+          },
+          {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
+                }
+              }
+            ]
+          }
+        ],
+      },
+}
